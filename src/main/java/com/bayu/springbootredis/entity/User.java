36f79c439f1ff53093463079139309134ef722fc
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-09-04 08:38:26 +0700

    add entity User

---
+++ src/main/java/com/bayu/springbootredis/entity/User.java
@@ -0,0 +1,26 @@
+package com.bayu.springbootredis.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "email")
+    private String email;
+}
